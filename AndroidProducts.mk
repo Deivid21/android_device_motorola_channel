489973ec4db5d73e78321db6686f553be5a161fe
Deivid21 <david.parra.ignacio@gmail.com>
2021-05-08 23:25:28 -0400

    channel: AEX-ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_channel.mk
+    $(LOCAL_DIR)/aosp_channel.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_channel-eng \
-    arrow_channel-user \
-    arrow_channel-userdebug
+    aosp_channel-eng \
+    aosp_channel-user \
+    aosp_channel-userdebug
